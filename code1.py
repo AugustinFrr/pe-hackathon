fc187c297ed49cc6b094c9d2efcbd1b25a68288e
Agathe Cotte <agathecotte42@gmail.com>
2023-10-31 14:40:02 +0100

    agathe1

--- code1.py
+++ code1.py
@@ -1,2 +1,15 @@
 #fichier de programme
 # test de changement
+
+##partie Agathe: faire une araignée pour un pays
+import matplotlib.pyplot as plt
+import numpy as np
+
+from matplotlib.patches import Circle, RegularPolygon
+from matplotlib.path import Path
+from matplotlib.projections import register_projection
+from matplotlib.projections.polar import PolarAxes
+from matplotlib.spines import Spine
+from matplotlib.transforms import Affine2D
+
+def ouvrir_table(fichier):
